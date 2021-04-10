383588f83c7d319dc9065eaf868a5e5aa447125c
stephan du Plooy <sdpphotography1@gmail.com>
2021-04-10 18:03:20 -0400

    Added and saved high scores to high scores page

--- finalscore.js
+++ finalscore.js
@@ -5,6 +5,9 @@ var score = localStorage.getItem("score");
 $(".finalscoreh1").append(score + "/100");
 
 function saveScore() {
+    var personname = document.querySelector(".personname").value;
+    localStorage.setItem("username", personname);
+    console.log(personname);
     localStorage.setItem("highscore1", score);
     console.log(localStorage.getItem("highscore1"))
 }
--- highscores.js
+++ highscores.js
@@ -1,2 +1,7 @@
 console.log(localStorage.getItem("highscore1"))
 
+    var score1 = localStorage.getItem("highscore1");
+    var name1 = localStorage.getItem("username");
+    $(".scoreboard").append(name1 + " : " + score1);
+
+
