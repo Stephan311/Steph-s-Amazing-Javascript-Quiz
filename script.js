ac64344102a3f2b755419c03bb535f07d9022c90
stephan du Plooy <sdpphotography1@gmail.com>
2021-04-09 20:18:14 -0400

    Linked quiz and index to each other on button click

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("HI")
+
+var starBtn = document.querySelector("#startBtn")
+
+function start() {
+    window.location.href = "file:///C:/Users/Stephan/Pictures/Course/Steph-s-Amazing-Javascript-Quiz/quiz.html";
+}
