148a76077310deddd85661f468000c89c89f5b82
stephan du Plooy <sdpphotography1@gmail.com>
2021-04-09 20:36:45 -0400

    Added function to set score values when a button is pressed

--- quizlocation.js
+++ quizlocation.js
@@ -1,1 +1,19 @@
 console.log("hi")
+
+
+//variables for correct and incorrect questions
+// var correct = 20;
+var wrong = 0;
+
+var finalscore = 0;
+
+//function to store correct answer score to local storage
+
+function question1(answer) {
+    console.log(answer);
+    if (parseInt(answer) === 20) {
+        (finalscore += 20);
+        console.log(finalscore);
+    }
+
+}
