914e4dfa6ed4149f49924b4f6b8a1abfab32048d
stephan du Plooy <sdpphotography1@gmail.com>
2021-04-09 21:01:05 -0400

    Added all questions and added local storage for score

--- quizlocation.js
+++ quizlocation.js
@@ -13,7 +13,51 @@ function question1(answer) {
     console.log(answer);
     if (parseInt(answer) === 20) {
         (finalscore += 20);
-        console.log(finalscore);
+        console.log(finalscore);  
+        localStorage.setItem("score", finalscore);
     }
 
 }
+
+//stores answers into local storage for Q2
+function question2(answer) {
+    console.log(answer);
+    if (parseInt(answer) === 20) {
+        (finalscore += 20);
+        console.log(finalscore);
+        localStorage.setItem("score", finalscore);
+    }
+
+}
+
+//stores answers into local storage for Q3
+function question3(answer) {
+    console.log(answer);
+    if (parseInt(answer) === 20) {
+        (finalscore += 20);
+        console.log(finalscore);
+        localStorage.setItem("score", finalscore);
+    }
+}
+
+//stores answers into local storage for Q4
+function question4(answer) {
+    console.log(answer);
+    if (parseInt(answer) === 20) {
+        (finalscore += 20);
+        console.log(finalscore);
+        localStorage.setItem("score", finalscore);
+    }
+}
+
+//stores answers into local storage for Q5
+function question5(answer) {
+    console.log(answer);
+    if (parseInt(answer) === 20) {
+        (finalscore += 20);
+        console.log(finalscore);
+        localStorage.setItem("score", finalscore);
+    }
+}
+
+localStorage.setItem("score", finalscore);
