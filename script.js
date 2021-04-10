87d5a4ed436645df0d691e4e22711827c913aafb
stephan du Plooy <sdpphotography1@gmail.com>
2021-04-09 20:22:45 -0400

    Created and Linked Quiz JS and CSS files

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("HI")
 
 var starBtn = document.querySelector("#startBtn")
 
+//when start btn is clicked the page quiz gets opened 
 function start() {
     window.location.href = "file:///C:/Users/Stephan/Pictures/Course/Steph-s-Amazing-Javascript-Quiz/quiz.html";
 }
