ac65fec301e784b25944dfebe5617cfed601569b
stephan du Plooy <sdpphotography1@gmail.com>
2021-04-10 16:57:56 -0400

    Added high scores page

--- finalscore.js
+++ finalscore.js
@@ -1,1 +1,10 @@
 console.log(localStorage.getItem("score"));
+
+var score = localStorage.getItem("score");
+
+$(".finalscoreh1").append(score + "/100");
+
+function saveScore() {
+    localStorage.setItem("highscore1", score);
+    console.log(localStorage.getItem("highscore1"))
+}
