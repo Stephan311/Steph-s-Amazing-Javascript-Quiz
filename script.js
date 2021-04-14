6b883a6ce86b1d68831fb734ee1807d8c70006ed
stephan du Plooy <sdpphotography1@gmail.com>
2021-04-14 10:12:41 -0400

    added correct links

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ var starBtn = document.querySelector("#startBtn")
 
 //when start btn is clicked the page quiz gets opened 
 function start() {
-    window.location.href = "file:///C:/Users/Stephan/Pictures/Course/Steph-s-Amazing-Javascript-Quiz/quiz.html";
+    window.location.href = "./quiz.html";
 }
